6c9052a974cef0b76662660cd6eb65e0c691966d
shawernseel <shawern101@gmail.com>
2024-08-07 14:30:49 -0700

    creating records

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Django abstracts database requests
+
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True) #django will auto timestamp; good to have
+    first_name = models.CharFeild(max_length=50)
+    last_name = models.CharFeild(max_length=50)
+    email = models.CharFeild(max_length=100)
+    phone = models.CharFeild(max_length=15)
+    address = models.CharFeild(max_length=100)
+    city = models.CharFeild(max_length=50)
+    state = models.CharFeild(max_length=50)
+    zipcode = models.CharFeild(max_length=50)
+
+
+
