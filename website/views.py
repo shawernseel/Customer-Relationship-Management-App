c7976dc01ff9d47a36f2d3ec1f61b73c9cd2d676
shawernseel <shawern101@gmail.com>
2024-08-07 09:32:37 -0700

    settup login routes

--- website/views.py
+++ website/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html', {})
+def home(request): #everytime a page is visited request is sent to backend here which is passed to view
+    return render(request, 'home.html', {}) #home page is returned from request
+
+
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
