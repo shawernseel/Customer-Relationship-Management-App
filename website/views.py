3fe89ef43effd80f8f5805b12240dda01634e74c
shawernseel <shawern101@gmail.com>
2024-08-09 12:25:22 -0700

    updating record

--- website/views.py
+++ website/views.py
@@ -87,4 +87,18 @@ def add_record(request):
         return render(request, 'add_record.html', {'form':form}) #pass form into webpage
     else:
         messages.success(request, "You Must Be Logged In To do That")
-        return redirect('home')        
+        return redirect('home')
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record) #populate addrecord form with current_record
+        if request.method == "POST":
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Record Has Been Updated")
+                return redirect('home')
+        return render(request, 'update_record.html', {'form':form})     
+    else:
+        messages.success(request, "You Must Be Logged In To do That")
+        return redirect('home')
