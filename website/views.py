56497aed4138ecf1ca62000cb7de2ccd115841e1
shawernseel <shawern101@gmail.com>
2024-08-09 11:32:18 -0700

    add record form setup

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
